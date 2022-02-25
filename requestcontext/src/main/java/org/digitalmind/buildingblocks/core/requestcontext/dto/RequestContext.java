acb986942a099cff3ef25f614734808658bf27ee
rio <razvan.ionescu@vodafone.com>
2022-02-25 23:09:58 +0200

    add client ip and x-forwarded-hosts to request context

--- requestcontext/src/main/java/org/digitalmind/buildingblocks/core/requestcontext/dto/RequestContext.java
+++ requestcontext/src/main/java/org/digitalmind/buildingblocks/core/requestcontext/dto/RequestContext.java
@@ -9,8 +9,12 @@ import java.util.Map;
 public interface RequestContext {
     public static String SERVER_IP_ADDRESS_ATTRIBUTE = "Server Ip";
     public static String SERVER_HOST_NAME_ATTRIBUTE = "Server Name";
+    public static String CLIENT_FORWARD_LIST_ATTRIBUTE = "Client X-Forwarded-For";
+    public static String CLIENT_IP_ADDRESS_ATTRIBUTE = "Client Ip";
     public static String SERVER_IP_ADDRESS_UNKNOWN = "unknown";
     public static String SERVER_HOST_NAME_UNKNOWN = "unknown";
+    public static String CLIENT_IP_ADDRESS_UNKNOWN = "unknown";
+    public static String CLIENT_FORWARD_LIST_UNKNOWN = "unknown";
 
     Map<String, Object> getDetails();
 
